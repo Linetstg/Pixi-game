fa94f62bfd22f620d137e57bf5b12dc9bf22c809
talyn <talyn@smart-it.com>
2025-10-20 14:00:54 +0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import path from 'path'; 
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080,
-    open: true,
+  base: '/Pixi-game/',  
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
   },
-});
+})
